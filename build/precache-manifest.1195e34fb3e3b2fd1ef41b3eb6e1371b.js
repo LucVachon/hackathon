dcb21f781907a9987f4e3a03894cdd2135837730
Vachon, Luc <Luc.Vachon@hmhco.com>
2019-11-14 15:00:17 -0500

    Committing built client

---
+++ build/precache-manifest.1195e34fb3e3b2fd1ef41b3eb6e1371b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4aeed0034f3c7a63ed59bb9e8010b52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b905cfa31d507d2b9345",
+    "url": "/static/css/main.05ff8b3a.chunk.css"
+  },
+  {
+    "revision": "98d9b5ac894108f48c0e",
+    "url": "/static/js/2.b212ba1e.chunk.js"
+  },
+  {
+    "revision": "b905cfa31d507d2b9345",
+    "url": "/static/js/main.c3a8ace2.chunk.js"
+  },
+  {
+    "revision": "e947955b356fd4e840ff",
+    "url": "/static/js/runtime-main.b1bd5645.js"
+  }
+]);
